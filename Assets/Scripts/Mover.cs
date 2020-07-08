bd43ca0b6ee090f3d3ae3eb0aa7624c4a0b50dbf
ThiagoFerreir4 <ferreirathiago.mail@gmail.com>
2020-07-08 19:23:49 +0300

    05. Raycasting implemented

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -6,10 +6,18 @@ using UnityEngine.AI;
 public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
-    
+
+    Ray lastRay;
+
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+        Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+        
         GetComponent<NavMeshAgent>().destination = target.position;
     }
 }
