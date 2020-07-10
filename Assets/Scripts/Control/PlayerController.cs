d4f1706def354230fa15b390b5030d3c7c372c7b
ThiagoFerreir4 <ferreirathiago.mail@gmail.com>
2020-07-10 11:59:20 +0300

    14. Raycasting for components

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -1,26 +1,54 @@
-﻿using RPG.Movement;
+﻿using RPG.Combat;
+using RPG.Movement;
 using UnityEngine;
 
 namespace RPG.Control
 {
-    public class PlayerController : MonoBehaviour {
-
-    private void Update() {
-         if (Input.GetMouseButton(0))
-        {
-            MoveToCursor();
-        }
-    }
-
-    private void MoveToCursor()
+    public class PlayerController : MonoBehaviour 
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        bool hasHit = Physics.Raycast(ray, out hit);
-        if (hasHit)
+
+        private void Update() 
         {
-            GetComponent<Mover>().MoveTo(hit.point);
+            InteractWithCombat();
+            InteractWithMovement();
+        }
+
+        private void InteractWithCombat()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+            foreach (RaycastHit hit in hits)
+            {
+                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+                if (target == null) continue;
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    GetComponent<Fighter>().Attack(target);
+                }
+            }
+        }
+
+        private void InteractWithMovement() 
+        {
+            if (Input.GetMouseButton(0))
+            {
+                MoveToCursor();
+            }
+        }
+
+        private void MoveToCursor()
+        {
+            RaycastHit hit;
+            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
+            if (hasHit)
+            {
+                GetComponent<Mover>().MoveTo(hit.point);
+            }
+        }
+
+        private static Ray GetMouseRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
     }
-}
 }
