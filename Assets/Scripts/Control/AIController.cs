189c072f0874d389dfd5a1535bca590663fde190
ThiagoFerreir4 <ferreirathiago.mail@gmail.com>
2020-07-14 16:11:10 +0300

    35. Ai Guarding Behaviour emplemented

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using RPG.Combat;
 using RPG.Core;
+using RPG.Movement;
 using UnityEngine;
 
 namespace RPG.Control
@@ -12,13 +13,19 @@ namespace RPG.Control
 
         Fighter fighter;
         Health health;
+        Mover mover;
         GameObject player;
+
+        Vector3 guardPosition;
 
         private void Start()
         {
             fighter = GetComponent<Fighter>();
             health = GetComponent<Health>();
+            mover = GetComponent<Mover>();
             player = GameObject.FindWithTag("Player");
+
+            guardPosition = transform.position;
         }
 
         private void Update()
@@ -31,7 +38,7 @@ namespace RPG.Control
             }
             else
             {
-                fighter.Cancel();
+                mover.StartMoveAction(guardPosition);
             }
         }
 
