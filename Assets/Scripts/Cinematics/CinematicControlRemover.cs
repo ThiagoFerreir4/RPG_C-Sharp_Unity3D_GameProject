3dc3c177775b7200630f1d6f351ff2ba3c8d5a2c
DESKTOP-9HIDD4K\Thiago Ferreira <ferreirathiago.mail@gmail.com>
2020-09-29 12:02:18 +0300

    46. Observer Pattern C#

---
+++ Assets/Scripts/Cinematics/CinematicControlRemover.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+    public class CinematicControlRemover : MonoBehaviour
+    {
+        private void Start()
+        {
+            GetComponent<PlayableDirector>().played += DisableControl;
+            GetComponent<PlayableDirector>().stopped += EnableControl;
+        }
+
+        private void DisableControl(PlayableDirector pd)
+        {
+            print("DisableControl");
+        }
+
+        private void EnableControl(PlayableDirector pd)
+        {
+            print("EnableControl");
+        }
+    }
+}
---
+++ Assets/Scripts/Cinematics/FakePlayableDirector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace RPG.Cinematics
+{
+    public class FakePlayableDirector : MonoBehaviour
+    {
+        public event Action<float> onFinish;
+
+        private void Start()
+        {
+            Invoke("OnFinish", 3f);
+        }
+
+        private void OnFinish()
+        {
+            onFinish(4.3f);
+        }
+    }
+}
